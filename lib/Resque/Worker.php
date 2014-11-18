e4c6101638abfbf9e78add18ddb189857a30c33a
mcfedr <mcfedr@gmail.com>
2014-11-18 12:39:49 +0200

    Surpress proc title warnings

--- lib/Resque/Worker.php
+++ lib/Resque/Worker.php
@@ -328,7 +328,7 @@ class Resque_Worker
 	{
 		$processTitle = 'resque-' . Resque::VERSION . ': ' . $status;
 		if(function_exists('cli_set_process_title')) {
-			cli_set_process_title($processTitle);
+			@cli_set_process_title($processTitle);
 		}
 		else if(function_exists('setproctitle')) {
 			setproctitle($processTitle);
